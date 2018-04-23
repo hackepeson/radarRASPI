d402a14deea104652d92a6006978b97bb56939d0
Hacke <hacke.peson@gmail.com>
2018-04-23 12:18:48 +0200

    updated

---
+++ udpSend/main.c
@@ -0,0 +1,61 @@
+/*
+    Simple udp client
+*/
+#include<stdio.h> //printf
+#include<string.h> //memset
+#include<stdlib.h> //exit(0);
+#include<arpa/inet.h>
+#include<sys/socket.h>
+ 
+#define SERVER "127.0.0.1"
+#define BUFLEN 512  //Max length of buffer
+#define PORT 8888   //The port on which to send data
+ 
+void die(char *s)
+{
+    perror(s);
+    exit(1);
+}
+ 
+int main(void)
+{
+    struct sockaddr_in si_other;
+    int s, i, slen=sizeof(si_other);
+    char buf[BUFLEN];
+    char message[BUFLEN];
+ 
+    if ( (s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+    {
+        die("socket");
+    }
+ 
+    memset((char *) &si_other, 0, sizeof(si_other));
+    si_other.sin_family = AF_INET;
+    si_other.sin_port = htons(PORT);
+     
+    if (inet_aton(SERVER , &si_other.sin_addr) == 0) 
+    {
+        fprintf(stderr, "inet_aton() failed\n");
+        exit(1);
+    }
+ 
+    while(1)
+    {
+	usleep(1000000);
+	static int i = 0;	
+
+	sprintf(message, "Kalle %d\n", i++);
+
+         
+        //send the message
+        if (sendto(s, message, strlen(message) , 0 , (struct sockaddr *) &si_other, slen)==-1)
+        {
+            die("sendto()");
+        }
+	printf("%s",message);
+
+    }
+ 
+    close(s);
+    return 0;
+}
